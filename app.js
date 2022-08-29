f6518ed87bc94862ec8f52e5c24050fe65802bae
Liseczkij2 <liseczkij2@gmail.com>
2022-08-29 21:27:51 +0300

    updete

---
+++ app.js
@@ -0,0 +1,42 @@
+document.getElementById('addTodo').addEventListener('click',  async () => {
+    const input = document.getElementById('todoText')
+    const title = input.value
+    if (title) {
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title, completed: false})
+        })
+        const todo = await res.json()
+        todoToHTML(todo)
+        input.value=''
+    }
+})
+
+async function getALLTodos() {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    const todos = await res.json()
+    console.log(todos)
+    todos.forEach(todo => todoToHTML(todo))
+
+}
+
+window.addEventListener('DOMContentLoaded', getALLTodos)
+
+function todoToHTML({id, completed, title}) {
+    const todoList = document.getElementById('todos')
+
+    todoList.insertAdjacentHTML('beforeend', `
+       <div class="form-check" id="todo ${id}">
+            <label for="" class="form-check-label">
+                <input type="checkbox" class="form-check-input" ${completed && 'checked'}>
+                 ${title}
+            </label>
+            <button type="button" class="btn-close" aria-label="Close" style="font-size: 10px"></button>
+        </div>
+    `)
+}
+
