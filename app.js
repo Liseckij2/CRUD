ca52d2131e821c03a305206d2af322fffb924a01
Liseczkij2 <liseczkij2@gmail.com>
2022-08-30 14:14:14 +0300

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.getElementById('addTodo').addEventListener('click',  async () => {
+
     const input = document.getElementById('todoText')
     const title = input.value
     if (title) {
@@ -29,14 +30,42 @@ window.addEventListener('DOMContentLoaded', getALLTodos)
 function todoToHTML({id, completed, title}) {
     const todoList = document.getElementById('todos')
 
-    todoList.insertAdjacentHTML('beforeend', `
-       <div class="form-check" id="todo ${id}">
+    todoList.insertAdjacentHTML('beforeend', ` 
+       <div class="form-check" id="todo${id}">
             <label for="" class="form-check-label">
-                <input type="checkbox" class="form-check-input" ${completed && 'checked'}>
-                 ${title}
+                <input onchange="toggleCompleteTodo(${id})" type="checkbox" class="form-check-input" ${completed && 'checked'}>
+
+                 ${title} 
             </label>
-            <button type="button" class="btn-close" aria-label="Close" style="font-size: 10px"></button>
+            <button onclick="deleteTodo(${id})"  type="button" class="btn-close" aria-label="Close" style="font-size: 10px"></button>
         </div>
     `)
 }
 
+
+async function deleteTodo(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: 'DELETE',
+        header: {
+            'Content-Type': 'application/json'
+        }
+        })
+    const data = await res.json()
+    console.log(data)
+
+    if (data) {
+        document.getElementById(`todo${id}`).remove()
+    }
+}
+ async function toggleCompleteTodo(id) {
+    const  completed = document.querySelector(`#todo${id} input`).checked
+     const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+         method: 'PATCH',
+         headers: {
+             'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({completed})
+     })
+     const data = await res.json()
+     console.log(data)
+ }
